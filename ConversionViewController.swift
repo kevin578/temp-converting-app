391e4a3ace2ebc91b35d1589145d1f328b7374ce
kevin578 <kevinbriggs1@gmail.com>
2017-08-11 09:42:28 -0400

    added input field

---
+++ ConversionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Kevin Briggs on 8/11/17.
+//  Copyright © 2017 Kevin Briggs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
